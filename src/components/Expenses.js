794585a30617206862dc4671062d391ae95d05eb
Felipe Lazzarini <felipe.mumei@gmail.com>
2022-05-20 19:40:59 -0300

    End of task 1, styling Expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
+import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
 
     return (
-        <div>
+        <div className='expenses'>
             <ExpenseItem title={props.exp[0].title} amount={props.exp[0].amount} date={props.exp[0].date} />
             <ExpenseItem title={props.exp[1].title} amount={props.exp[1].amount} date={props.exp[1].date} />
             <ExpenseItem title={props.exp[2].title} amount={props.exp[2].amount} date={props.exp[2].date} />
