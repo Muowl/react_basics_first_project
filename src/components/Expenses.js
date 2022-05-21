eff9896fa17400f1599de6f9ff61e336c039b8be
Felipe Lazzarini <felipe.mumei@gmail.com>
2022-05-21 00:44:35 -0300

    Added Cards (concept of children props)

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
 import './Expenses.css'
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 function Expenses(props) {
 
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem title={props.exp[0].title} amount={props.exp[0].amount} date={props.exp[0].date} />
             <ExpenseItem title={props.exp[1].title} amount={props.exp[1].amount} date={props.exp[1].date} />
             <ExpenseItem title={props.exp[2].title} amount={props.exp[2].amount} date={props.exp[2].date} />
             <ExpenseItem title={props.exp[3].title} amount={props.exp[3].amount} date={props.exp[3].date} />
-        </div>
+        </Card>
     );
 }
 
